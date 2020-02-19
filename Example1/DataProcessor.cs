78b653e6b3be3dadde2bfd085cccb2c3f707df23
Davis Mosenkovs <davikovs@gmail.com>
2020-02-20 00:56:35 +0200

    Added first example

---
+++ Example1/DataProcessor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+
+namespace Example1
+{
+    class DataProcessor
+    {
+        private int count = 0;
+        private int sum = 0;
+        private object lockobject = new object();
+
+        private int GetCompulationallyExpensiveCalculationResult(int p)
+        {
+            Thread.Sleep(5000);
+            return p * 5;
+        }
+        private void DoCalculationAndUpdateResultsInterlocked(int p) {
+            lock (lockobject)
+            {
+                sum += GetCompulationallyExpensiveCalculationResult(p);
+                count++;
+            }
+        }
+        public void DoProcessing(int p) {
+            if (p < 0 || p > 100) {
+                throw new ArgumentOutOfRangeException("Incorrect parameter");
+            }
+            DoCalculationAndUpdateResultsInterlocked(p);
+        }
+        public string GetResults() {
+            return string.Format("count = {0}, sum = {1}", count, sum);
+        }
+    }
+}
