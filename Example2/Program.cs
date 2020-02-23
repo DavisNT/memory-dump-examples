a5fe8539ff875dd859512474c8ec345145a62af6
Davis Mosenkovs <davikovs@gmail.com>
2020-02-23 22:38:34 +0200

    Added Example2

---
+++ Example2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+
+namespace Example2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread[] threadsArray = new Thread[100];
+            var dp = new DataProcessor();
+
+            Console.WriteLine("Starting processing in 100 threads...");
+            for (int i = 0; i < 100; i++)
+            {
+                var i2 = i;
+                threadsArray[i] = new Thread(() => dp.DoProcessing(i2));
+                threadsArray[i].Start();
+            }
+            Console.WriteLine("Threads started.");
+
+            for (int i = 0; i < 100; i++)
+            {
+                threadsArray[i].Join();
+            }
+            Console.WriteLine("Processing done. Result is " + dp.GetResults());
+        }
+    }
+}
